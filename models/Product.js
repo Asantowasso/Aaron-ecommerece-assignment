5c5c111e9e6b05c596d291d06a1255693ef77938
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-12 16:39:02 -0500

    created models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+
+      }
+
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+
+      }
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+        references: {
+          model: 'Category',
+          key: 'id',
+        }
+    }
   },
   {
     sequelize,
